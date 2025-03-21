5e2cef876c548d3bb747b5d620913f57bb6268c5
Austin Pratt <austinjpratt@yahoo.com>
2025-03-21 10:12:50 -0500

    Update server.js

--- raspberrypi-setup/server.js
+++ raspberrypi-setup/server.js
@@ -1,48 +1,19 @@
-const express = require('express');
-const multer = require('multer');
-const fs = require('fs');
-const path = require('path');
+var http = require('http');
+var formidable = require('formidable');
 
-const app = express();
-const upload = multer({ dest: 'uploads/' });
-
-app.use(express.static('public'));
-app.use(express.json());
-
-// Upload endpoint
-app.post('/upload', upload.single('file'), (req, res) => {
-  res.json({ fileName: req.file.filename, originalName: req.file.originalname });
-});
-
-// Download endpoint
-app.get('/download/:filename', (req, res) => {
-  const filePath = path.join(__dirname, 'uploads', req.params.filename);
-  res.download(filePath);
-});
-
-// Delete endpoint
-app.delete('/delete/:filename', (req, res) => {
-  const filePath = path.join(__dirname, 'uploads', req.params.filename);
-  fs.unlink(filePath, (err) => {
-    if (err) {
-      return res.status(500).json({ error: 'File not found' });
-    }
-    res.json({ message: 'File deleted' });
-  });
-});
-
-// List files endpoint
-app.get('/files', (req, res) => {
-  const directoryPath = path.join(__dirname, 'uploads');
-  fs.readdir(directoryPath, (err, files) => {
-    if (err) {
-      return res.status(500).json({ error: 'Unable to scan files' });
-    }
-    res.json(files);
-  });
-});
-
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      res.write('File uploaded');
+      res.end();
+    });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(8080);
