242f544af397dab8de7504e97006422734c315c1
Austin Pratt <austinjpratt@yahoo.com>
2025-03-21 12:22:46 -0500

    Create setup.sh

---
+++ latest/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Update and the system
+sudo apt update
+
+# Download and install fnm:
+curl -o- https://fnm.vercel.app/install | bash
+# Download and install Node.js:
+fnm install 22
+# Verify the Node.js & npm version:
+node -v # Should print "v22.14.0".
+npm -v # Should print "10.9.2".
+
+# Initialize a new Node.js project
+npm init -y
+
+# Install formidable npm package
+npm install formidable
+
+# Create the uploads directory
+mkdir fileupload
+
+# Start the server
+node server.js
